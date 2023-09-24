c86775d0d83db43c31fde812acfb36715fbe4bad
Kbozz <Luboteroa@unal.edu.co>
2023-09-24 18:06:03 -0500

    .

--- Assets/Scripts/Managers/CinematicManager.cs
+++ Assets/Scripts/Managers/CinematicManager.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 
 public class CinematicManager : MonoBehaviour
 {
-    [SerializeField] private Animator transition;
-    private float transitionTime = 3.0f;
-    public Dialogue dialogue;
-    public GameObject Cinematica;
-
     #region SINGLETON
     public static CinematicManager Instance{ get; private set; }
     private void Awake()
@@ -28,34 +23,4 @@ public class CinematicManager : MonoBehaviour
         }
     }
     #endregion
-
-    private void Start()
-    {
-        transitionTime = FindObjectOfType<SceneLoader>().transitionTime;
-    }
-    
-    public void LoadNextObject()
-    {
-        StartCoroutine(LoadObject());
-    }
-
-    IEnumerator LoadObject()
-    {
-        transition.SetTrigger("Start");
-
-        yield return new WaitForSeconds(transitionTime);
-
-        Cinematica.SetActive(true);
-
-        transition.SetTrigger("Start");
-
-        yield return new WaitForSeconds (2.0f);
-
-        DialogueTrigger();
-    }
-
-    public void DialogueTrigger()
-    {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-    }
 }
